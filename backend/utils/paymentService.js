b8e8436363aae02d88ef41a281f78066d4760173
vasudha <vasudhabhatkp@gmail.com>
2026-04-16 23:12:45 +0530

    Calcs

--- backend/utils/paymentService.js
+++ backend/utils/paymentService.js
@@ -1,5 +1,4 @@
 
-// Calculate non-refundable service charge (percentage of total)
 export const calculateServiceCharge = (totalAmount) => {
   const percentage = parseFloat(process.env.SERVICE_CHARGE_PERCENTAGE) || 2;
   return (totalAmount * percentage) / 100;
